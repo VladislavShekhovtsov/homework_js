ed1927714e4da2a291b901ca82c167e3c9e4664d
VladShekhovtsov <vlad.shekhovtsov93@gmail.com>
2023-05-25 19:21:59 +0300

    исправление ошибки

---
+++ 03/home_work_03.js
@@ -0,0 +1,73 @@
+{ // Приветствие
+    var userName = prompt("Введите ваше имя:");
+
+        alert("Привет, " + userName + "! Добро пожаловать!");
+
+}
+
+
+{ // Блин
+    var userStr = prompt("Введите строку:");
+
+    var newString = userStr.split(",").join(", блин");
+
+    alert(newString);
+}
+
+
+{   // Регистр
+
+    let str = "cANBerRa";
+
+    let result  = str.slice() [0].toUpperCase() + str.slice(1).toLowerCase();
+
+    console.log(result);
+}
+
+{   // Кол-во слов в строке
+    let str = prompt("Введите строку:");
+
+    let division = str.split(" ");
+
+    let numberWords = division.length;
+
+    alert(numberWords);
+
+}   
+
+{   //  ФИО
+    let surnameName = prompt("Введите фамилию:");
+
+    let name = prompt("Введите имя:");
+
+    let middleName = prompt("Введите отчество:");
+
+    surnameName = surnameName.trim();
+    name = name.trim();
+    middleName = middleName.trim();
+
+    surnameName = surnameName.slice() [0].toUpperCase() + surnameName.slice(1).toLowerCase();
+    name = name.slice() [0].toUpperCase() + name.slice(1).toLowerCase();
+    middleName = middleName.slice() [0].toUpperCase() + middleName.slice(1).toLowerCase();
+
+    let fullName = surnameName + " " + name + " " + middleName;
+
+    alert(fullName);
+      
+}
+
+{   //  Замена слова
+    let str = "Було жарко. Василь пив пиво вприкуску з креветками";
+let result = str.split("пиво").join("чай");
+
+alert(result) 
+
+}
+
+{
+
+    let str = "якийсь текст, в якому є один тег <br /> і всяке інше"
+let result = str.indexOf("<>").slice(33, 38)
+
+console.log(result) 
+}
